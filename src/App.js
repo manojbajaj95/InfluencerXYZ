a5808f18e786c5596c15d7207fcfdb5e850c3595
Manoj Bajaj <manojbajaj95@gmail.com>
2020-07-17 16:52:26 +0530

    Added dashboard initial comit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from "./components/Home/HomeDefault";
 import Influencers from "./components/Home/HomeInfluencers";
 import Brands from "./components/Home/HomeBrands";
 import FormPage from "./components/Registration/RegistrationInfluencer";
-
+import Dashboard from "./components/Dashboard/DashboardPage";
 
 function App() {
   return (
@@ -17,6 +17,7 @@ function App() {
           <Route exact path="/influencers" component={Influencers} />
           <Route exact path="/form" component={FormPage} />
           <Route exact path="/brands" component={Brands} />
+          <Route exact path="/dashboard" component={Dashboard} />
         </Switch>
       </BrowserRouter>
     </div>
